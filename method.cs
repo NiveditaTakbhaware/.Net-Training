189e8b94c7c70411e201cf6a06dcbf8593fcfa3e
Nivedita Takbhaware <nikniv16@gmail.com>
2020-03-25 21:23:43 +0530

    11th commit

---
+++ method.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1method
+{
+    class Program
+    {
+         void arm()
+        {
+            int num, rev = 0, org, d;
+            Console.WriteLine("enter the number");
+            num = int.Parse(Console.ReadLine());
+            org = num;
+            while(num!=0)
+            {
+                d = num % 10;
+                num = num / 10;
+                rev = rev+ d * d * d;
+            }
+            if (rev == org)
+                Console.WriteLine("armstrong");
+            else
+                Console.WriteLine("not armstrong");         
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.arm();
+            Console.ReadKey();
+
+        
+        }
+       
+        
+
+
+        
+    
+   
+
+    }
+}
