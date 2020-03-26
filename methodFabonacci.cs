2d461ddd83e87a576e29d1a6d0d2cae3f0f7a1f9
Nivedita Takbhaware <nikniv16@gmail.com>
2020-03-26 22:26:38 +0530

    17th commit

---
+++ methodFabonacci.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6methodfabonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0, n2 = 1, n3;
+            Console.WriteLine("
+                +)
+
+        }
+
+    }
+}
