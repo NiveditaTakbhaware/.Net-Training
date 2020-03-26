6233375846cd890b4275e4d3c8c86c88f3464aaa
Nivedita Takbhaware <nikniv16@gmail.com>
2020-03-26 22:29:28 +0530

    20th commit

---
+++ Outmethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7outmethod
+{
+    class Program
+    {
+        static void show(out int n)
+        {
+            Console.WriteLine("enter the number");
+            n = Convert.ToInt32(Console.ReadLine());
+            n++;
+            Console.WriteLine("number is " + n);
+        }
+        static void Main(string[] args)
+        {
+            int k;
+            show(out k);
+            
+            Console.ReadKey();
+        }
+    }
+}
