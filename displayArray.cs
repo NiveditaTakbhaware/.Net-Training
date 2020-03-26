948ab66d96dabca9b9a4f2f96b078d1c1e8edbe2
Nivedita Takbhaware <nikniv16@gmail.com>
2020-03-26 22:24:54 +0530

    15th commit

---
+++ displayArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9displayarray
+{
+    class Program
+    {
+        static int[] reverse(params int[] a)
+        {
+            int j;
+            Console.WriteLine("array");
+            for(j=0;j<a.Length;j++)
+            {
+                Console.WriteLine(a[j]);
+            }
+           
+            return a;                                 
+        }        
+        static void Main(string[] args)
+        {
+            int i,j;
+            int[] res = new int[5];
+            int[] arr = new int[5];
+            Console.WriteLine("enter elements");
+            for(i=0;i<=4;i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+           res=reverse(arr);
+            Console.WriteLine("reverse");
+            for (j = res.Length - 1; j >= 0; j--)
+            {
+                Console.WriteLine(res[j]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
