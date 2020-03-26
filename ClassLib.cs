ffb776f6d329ae92a2f02c1b3db17f99fbc57d10
Nivedita Takbhaware <nikniv16@gmail.com>
2020-03-26 23:09:14 +0530

    29th commit

---
+++ ClassLib.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exampleClassLibrary
+{
+    public class Calculate
+    {
+        public int  num1{ get; set; }
+       
+        public string Remainder(int n1)
+        {
+            num1 = n1;
+            if (num1 % 2 == 0)
+            
+                 return "even number";
+            
+            else
+                 return "odd number"; 
+        } 
+
+    }
+   
+}
