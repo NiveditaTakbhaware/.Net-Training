bb20fb2eb16dc468fd9f51adf2fc9b72043e0b79
Nivedita Takbhaware <nikniv16@gmail.com>
2020-03-26 22:30:16 +0530

    21st commit

---
+++ ParamMethod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8parammethods
+{
+    class Program
+    {
+        void cal(params int[] val)
+        {
+            int i;
+            for (i=0;i<val.Length;i++)
+            {
+                Console.WriteLine("array:" + val[i]);
+            }
+           // return ;
+        }
+        static void Main(string[] args)
+        {
+            Program p1 = new Program();
+            p1.cal(2, 3, 4, 5, 6, 7);
+            Console.ReadKey(); 
+        }
+    }
+}
