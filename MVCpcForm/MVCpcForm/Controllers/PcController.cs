5800d96762bab877bebd7ade90179c159c5725ff
Nivedita Takbhaware <nikniv16@gmail.com>
2020-08-20 16:56:46 +0530

    pc form mvc

---
+++ MVCpcForm/MVCpcForm/Controllers/PcController.cs
@@ -0,0 +1,90 @@
+﻿using MVCpcForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace MVCpcForm.Controllers
+{
+    public class PcController : Controller
+    {
+        static string cnstrng = ConfigurationManager.ConnectionStrings["mvcConstring"].ConnectionString;
+        SqlConnection con = new SqlConnection(cnstrng);
+        // GET: Pc
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult create(PcModel p)
+        {
+            if (p.img!= null)
+
+            {
+                Stream fs = p.img.InputStream;
+
+                BinaryReader br = new BinaryReader(fs);
+
+                byte[] bytes = br.ReadBytes((Int32)fs.Length);
+
+                SqlCommand cmd = new SqlCommand("Insert into pcform(brandname,category,screensize,memory,os,color,img,name)values(@bd,@cat,@sc,@mem,@os,@col,@img,@name)", con);
+                cmd.Parameters.AddWithValue("@bd", p.brandname);
+                cmd.Parameters.AddWithValue("@cat", p.category);
+                cmd.Parameters.AddWithValue("@sc", p.screensize);
+                cmd.Parameters.AddWithValue("@mem", p.memory);
+                cmd.Parameters.AddWithValue("@os", p.os);
+                cmd.Parameters.AddWithValue("@col", p.color);
+                cmd.Parameters.AddWithValue("@img", bytes);
+                cmd.Parameters.AddWithValue("@name", p.name);
+
+                con.Open();
+
+                int i=Convert.ToInt32(cmd.ExecuteNonQuery());
+                con.Close();
+                if(i==1)
+                {
+                    ViewBag.msg = "inserted done !";
+                }
+            }
+
+            return View();
+        }
+        public ActionResult list(PcModel pc)
+        {
+            //con.Open();
+            string q = "select id,brandname,category,screensize,memory,os,color,img,name from pcform";
+            SqlCommand cmd = new SqlCommand(q, con);
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sd.Fill(ds, "info");
+            //con.Close();
+            foreach (DataRow dr in ds.Tables["info"].Rows)
+            {
+                pc.id = Convert.ToInt32(dr["id"]);
+                pc.brandname =(bd)dr["brandname"];
+                pc.category = dr["category"].ToString();
+                pc.screensize = dr["screensize"].ToString();
+                pc.memory = dr["memory"].ToString();
+                pc.os = (OS)dr["os"];
+                pc.color = dr["color"].ToString();
+                byte[] pic = (byte[])dr["img"];
+                string p = Convert.ToBase64String(pic, 0, pic.Length);
+                string pi = "data:image/png;base64," + p;
+                ViewBag.imge = pi;
+                pc.name = dr["name"].ToString();
+                //ViewBag.name = pc.name;
+            }
+            return View();
+        }
+    }
+}
---
+++ MVCpcForm/MVCpcForm/Models/PcModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCpcForm.Models
+{
+    public class PcModel
+    {
+        public int id { get; set; }
+        public bd brandname { get; set; }
+        public string category { get; set; }
+        public string screensize { get; set; }
+        public string memory { get; set; }
+        public OS os { get; set; }
+        public string color { get; set; }
+        public HttpPostedFileBase img { get; set; }
+        public string name { get; set; }
+    }
+    public enum bd
+    {
+        hp, samsung, DELL, sony, LG
+    }
+    public enum OS
+    {
+        windows, linux, dos
+    }
+}
